b819a2c24582a35c8e51ffc10bde42618ea82810
Jun-T-git <jun0303t@icloud.com>
2021-12-04 13:03:42 +0900

    環境変数対応，表示方法変更，オフシーズンバグ修正

--- app/tweet_todays_best9.py
+++ app/tweet_todays_best9.py
@@ -1,10 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
+from operator import mul
 import datetime
 import re
-from operator import mul
 import tweepy
 import sys
+import os
+from dotenv import load_dotenv
 
 # dateで受け取った日に開催された各試合の出場成績のリンクをリスト形式で返す
 # date: 試合日（例：2021-09-24）
@@ -12,6 +14,9 @@ def fetch_game_links(date):
   params = { 'date': date }
   schedule_page = requests.get('https://baseball.yahoo.co.jp/npb/schedule', params=params)
   soup_schedule = BeautifulSoup(schedule_page.text, 'html.parser')
+  game_date = soup_schedule.find('h1', class_='bb-head02__title').text # 9月24日（木）
+  if (str(int(date[-5:-3])) not in game_date[:2]) or (str(int(date[-2:])) not in game_date[2:5]):
+    return [] # 取得リンクの日付がリクエストの日付と異なっていた場合(オフシーズン対策)
   game_link_elms = soup_schedule.find_all('a', class_='bb-score__content')
   game_links = list(map(lambda x: x['href'].replace('index', 'stats'), game_link_elms))
   return game_links
@@ -19,7 +24,7 @@ def fetch_game_links(date):
 # statsで受け取った成績から総合スコアを計算する
 def calc_batter_score(stats):
   # 打数，　得点，　安打，　打点，　三振，　四球，　死球，　犠打，　盗塁，　失策，　本塁打
-  eval_list = [-0.05, 0, 4, 2.5, 0, 1.5, 1, 1, 1.5, -2, 1] # statsの第3要素以降と加重和をとる
+  eval_list = [0, 0, 4, 2.5, 0, 1.5, 0.5, 0.5, 1.5, -2, 1] # statsの第3要素以降と加重和をとる
   score = sum(list(map(mul, stats[3:], eval_list)))
   return score
 
@@ -59,7 +64,11 @@ def fetch_pitcher_stats(game_link):
     stats_html = row.find_all('td', class_='bb-scoreTable__data')
     if stats_html:
       stats = list(map(lambda x: x.text, stats_html))[:14]
-      stats[0] = '投'
+      if '\n' in stats[0]: # 勝負HSがどの選手にもついていない試合の場合（引き分けなど）
+        stats.insert(0, '投')
+        stats = stats[:14]
+      else:
+        stats[0] = '投'
       stats[1] = stats[1].replace('\n', '')
       stats[3] = float(stats[3]) # 投球回をfloatに変換
       stats[4:] = list(map(int, stats[4:])) # 投手成績をintに変換
@@ -93,9 +102,9 @@ def select_best9(batter_stats, pitcher_stats):
 
 # ツイート内容（ベストナイン）
 def tweet_content_best9(date, best9_stats):
-  content = f'{date}のベストナイン\n\n'
+  content = f'{date[5:]}のベストナイン\n\n'
   for (position, stats) in best9_stats.items():
-    row = f'【{position}】'
+    row = f'{position}. '
     if position == '投':
       row += f'{stats[1]}（{stats[3]}-{stats[-2]}）'
     else:
@@ -105,7 +114,7 @@ def tweet_content_best9(date, best9_stats):
 
 # ツイート内容（野手成績）
 def tweet_content_batter(date, all_batter_stats):
-  content = f'{date}の野手成績ランキング\n\n'
+  content = f'{date[5:]}の野手成績ランキング\n\n'
   rank = 1
   for stats in all_batter_stats:
     content += f'{rank}. {stats[1]}（{stats[3]}-{stats[5]}-{stats[6]}） {round(stats[-1], 1)}点\n'
@@ -116,7 +125,7 @@ def tweet_content_batter(date, all_batter_stats):
 
 # ツイート内容（投手成績）
 def tweet_content_pitcher(date, all_pitcher_stats):
-  content = f'{date}の投手成績ランキング\n\n'
+  content = f'{date[5:]}の投手成績ランキング\n\n'
   rank = 1
   for stats in all_pitcher_stats:
     content += f'{rank}. {stats[1]}（{stats[3]}-{stats[-2]}） {round(stats[-1], 1)}点\n'
@@ -125,11 +134,13 @@ def tweet_content_pitcher(date, all_pitcher_stats):
       break
   return content
 
+# ツイートする
 def tweet(tweet_content):
-  CK="e7VK69thvuKi32JviHeGQ7v04"
-  CS="jmf7pcYqLNpxIe91JlGIMQgQavzFpWjL3FIVNFioxLOPdEAdET"
-  AT="1441398495194140679-zrIDft039fiYe2WqAg14nXiWMNxxxb"
-  AS="Ua1dfVHyH9KpFbYnvuRoO0RV2ocqNdAhDuZTViLd6QsXQ"
+  load_dotenv() # .env読み込み
+  CK=os.environ['API_KEY']
+  CS=os.environ['API_SECRET_KEY']
+  AT=os.environ['ACCESS_TOKEN']
+  AS=os.environ['ACCESS_SECRET_TOKEN']
   # Twitterオブジェクトの生成
   auth = tweepy.OAuthHandler(CK, CS)
   auth.set_access_token(AT, AS)
@@ -138,10 +149,10 @@ def tweet(tweet_content):
   api.update_status(tweet_content)
 
 if __name__ == '__main__':
-  d_today = datetime.date.today()
-  # print(d_today)
+  d_today = str(datetime.date.today())
+  print(d_today)
   game_links = fetch_game_links(d_today) # 試合リンクの取得
-  # print(game_links)
+  print(game_links)
   all_batter_stats = []
   all_pitcher_stats = []
   for game_link in game_links:
@@ -153,8 +164,8 @@ if __name__ == '__main__':
     pitcher_stats = fetch_pitcher_stats(game_link)
     all_pitcher_stats.extend(pitcher_stats)
     all_pitcher_stats = sorted(all_pitcher_stats, key=lambda x: x[-1], reverse=True)
-  # print(all_batter_stats)
-  # print(all_pitcher_stats)
+  print(all_batter_stats)
+  print(all_pitcher_stats)
   if len(all_batter_stats) == 0 or len(all_pitcher_stats) == 0:
     print("No data")
     sys.exit()
@@ -165,8 +176,3 @@ if __name__ == '__main__':
   tweet(best9_content)
   tweet(batter_content)
   tweet(pitcher_content)
-  print("ツイートしました")
-  # print(best9_stats)
-  # print(tweet_content_best9(d_today, best9_stats))
-  # print(tweet_content_batter(d_today, all_batter_stats))
-  # print(tweet_content_pitcher(d_today, all_pitcher_stats))
